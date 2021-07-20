7489c6a01f0d378a38cdcdbf3ba99f95307589b9
Daniel <46201432+TechGeekGamer@users.noreply.github.com>
2021-07-20 10:43:15 -0700

    Create config.template.ts

---
+++ config.template.ts
@@ -0,0 +1,16 @@
+const config: Config  = {
+    "bot":{
+        "token":""
+    },
+    "tickets":{
+        "guild_id":`${1234567890}`,
+        "message":"",
+        "openCategory":"1234567890",
+        "archivedCategory":"1234567890",
+        "topic":""
+    },
+    "users":{
+        "whitelisted":[]
+    }
+}
+export default config;
